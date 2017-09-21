d358902d6c6527ead911d66f5171cdfaf94dfa5e
Uilian Ries <uilianries@gmail.com>
2017-09-21 10:36:49 -0300

    Update to version 4.1.2
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/main.c
+++ test_package/main.c
@@ -13,9 +13,6 @@ int main(int argc, char **argv)
     char errbuf[PCAP_ERRBUF_SIZE] = {0};
     bpf_u_int32 netp = 0;
     bpf_u_int32 maskp = 0;
-    pcap_if_t *alldevs = NULL;
-    pcap_if_t *it = NULL;
-    const char* dev = NULL;
 
     dev = pcap_lookupdev(errbuf);
     if (dev != NULL) {
@@ -23,21 +20,6 @@ int main(int argc, char **argv)
             fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
         }
     }
-
-    if (pcap_findalldevs(&alldevs, errbuf) == -1) {
-        fprintf(stderr,"Couldn't execute pcap_findalldevs: %s\n", errbuf);
-	return EXIT_SUCCESS;
-    }
-
-    for (it = alldevs; it != NULL; it = it->next) {
-        printf("NAME: %s\n", it->name);
-        if (it->description) {
-            printf("DESCRIPTION: %s\n", it->description);
-        }
-        printf("IS LOOPBACK: %s\n\n", (it->flags & PCAP_IF_LOOPBACK) ? "YES":"NO");
-    }
-
-    pcap_freealldevs(alldevs);
 
     return EXIT_SUCCESS;
 }
